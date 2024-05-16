0de5c194777b4bdccfd3ca2e16eee16d830414be
Kristofer_Almeros <kristofer.almeros@hotmail.com>
2024-05-16 21:29:18 +0200

    ändrat prenumerationer-endpoint, samt flyttat funktionen till userRouter och tagit bort subscribersRouter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const user = express.Router()
 const { createUser } = require('../functions/userFunctions')
 const getUserById = require('../functions/getUserById')
+const {checkSusbscription, newSubscriber} = require("../functions/subscribersFunctions")
+const {checkUserID, checkChannelID} = require("../functions/channelFunctions")
 
 //BaseURL '/api/user'
 user.post('/signup', async (req, res) => {
@@ -70,6 +72,33 @@ user.get('/:id', async (req, res) => {
 
 user.post('/login', (req, res) => {
   res.json({ msg: 'logged in ' })
+})
+
+// Skapa prenumerationer
+user.post("/:userId/sub/:channelId", async (req, res) => {
+  const { userId, channelId } = req.params
+
+  try {
+      const userExists = await checkUserID(userId)
+      if (!userExists) {
+          return res.status(400).json({message: "User does not exist"})
+      }
+
+      const channelExists = await checkChannelID(channelId) 
+      if (!channelExists) {
+          return res.status(400).json({message: "Channel does not exist"})
+      }
+
+      const subscriptionExists = await checkSusbscription(userId, channelId)
+      if (subscriptionExists) {
+          return res.status(500).json({message: "Subscription already exists"})
+      }
+
+      const subscription = await newSubscriber(userId, channelId)
+      return res.status(201).json({message: `Successfully subscribed user with id ${userId} to channel with id ${channelId}`, subscription})
+  } catch (error) {
+      return res.status(500).json({message: "Failed to subscribe"})
+  }
 })
 
 module.exports = user
